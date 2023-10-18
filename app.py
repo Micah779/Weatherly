b3b54c09ff99537f380cabf4e906e7bb5209e3d4
Micah779 <contactmaldrich@gmail.com>
2023-10-18 16:27:10 -0400

    basic styling updates

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
+from flask import render_template # allows us to render HTML files
 
 app = Flask(__name__) # application instance
 
 @app.route('/') # application route
 def index(): # view function
-    return 'Weather App!'
-
-@app.route('/<name>')
-def print_name(name):
-    return 'Hi, {}'.format(name)
+    return render_template('index.html') # renders the index.html file as a view function
 
 if __name__ == '__main__':
     app.run() # programmatitically running Flask

22ec19b2b73af5ac4cda5892a7a6530f257d7a4c
Micah779 <contactmaldrich@gmail.com>
2023-10-20 12:20:16 -0400

    removed base, isolated the route, get and post req

--- app.py
+++ app.py
@@ -5,29 +5,26 @@ from flask import render_template, request # allows us to render HTML files
 
 app = Flask(__name__) # application instance
 
-@app.route('/') # application route
+api_key = "cc631ac49cd8a748900eedaba0033755"
+
+@app.route('/', methods=["POST", 'GET']) # application route
 def index(): # view function
-    return render_template('index.html') # renders the index.html file as a view function
 
-@app.route('/results', methods=['POST'])
-def render_results():
-    zipcode = request.form['zipCode']
+    # initializing data variables outside of the conditional statement so that they can be accessed
+    location, temp, feels_like, weather = None, None, None, None
 
-    api_key = get_api_key()
-    data = get_weather_results(zipcode, api_key)
+    if request.method == "POST":
+        zipcode = request.form['zipCode']
+        data = get_weather_results(zipcode, api_key)
 
-    # variables
-    temp = "{0:.2f}".format(data["main"]["temp"])
-    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
-    weather = data["weather"][0]["main"]
-    location = data["name"]
+        # variables
+        temp = "{0:.2f}".format(data["main"]["temp"])
+        feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+        weather = data["weather"][0]["main"]
+        location = data["name"]
 
-    return render_template('base.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
-
-def get_api_key():
-    config = configparser.ConfigParser()
-    config.read('config.ini') # read in the config.ini file
-    return config['weatherly']['api'] # getting the api key, now we can run 'get_api_key()'
+    # renders the index.html file as a view function with all of the variables from the api data
+    return render_template('index.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
 
 # function to take in a zipcode and api key to return weather results
 def get_weather_results(zipcode, api_key):
